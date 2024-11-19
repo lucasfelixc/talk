aad6508b45a7a5a92254f928be0487b9eeba9077
lucasfelixc <lucasfelixdev@gmail.com>
2024-11-19 08:27:05 -0300

    Add typeorm

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,22 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MessagesModule } from 'src/messages/messages.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [MessagesModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5433,
+      username: 'postgres',
+      database: 'postgres',
+      password: '123456',
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+    MessagesModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
