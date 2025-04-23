5b34fb0ea979c16c02bb1315075e06819878f70e
lucasfelixc <lucasfelixdev@gmail.com>
2025-04-23 09:13:25 -0300

    Wip

---
+++ src/common/interceptors/add-header.interceptor.ts
@@ -0,0 +1,15 @@
+import type {
+  CallHandler,
+  ExecutionContext,
+  NestInterceptor,
+} from '@nestjs/common';
+import { Observable } from 'rxjs';
+
+export class AddHeaderInterceptor implements NestInterceptor {
+  intercept(
+    context: ExecutionContext,
+    next: CallHandler<any>,
+  ): Observable<any> | Promise<Observable<any>> {
+    return next.handle();
+  }
+}
